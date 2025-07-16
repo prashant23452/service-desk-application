922ca3ab643bca6af5b489622c88051dc054b06c
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2025-07-16 12:01:53 +0530

    c2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import RequireAuth from './auth/RequireAuth';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Dashboard from './pages/user/Dashboard';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 function App() {
   return (
-    <>
-    Hi!
-    </>
+    <Router>
+      <div className='min-h-screen bg-[oklch(0.34_0.07_200.84)] text-white'>
+        <Navbar/>
+        <div className='container mx-auto px-4 py-6'>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route element={<RequireAuth/>}>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+            </Route>
+            <Route element={<RequireAuth/>}>
+              <Route path="/admin" element={<AdminDashboard/>}/>
+            </Route>
+            
+          </Routes>
+          
+        </div>
+      </div>
+    
+    </Router>
   );
 }
 
---
+++ src/auth/RequireAdmin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RequireAdmin() {
+  return (
+    <div>RequireAdmin</div>
+  )
+}
+
+export default RequireAdmin
